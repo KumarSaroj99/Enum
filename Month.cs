6ed0c1c2b5bcb50d3681a51bb3221f0b4258ee9d
KumarSaroj99 <sarojkumar.panda@aurionpro.com>
2024-07-16 15:26:25 +0530

    Add files via upload

---
+++ Month.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POConEnum
+{
+    //enum is a special "class" that represents a group of constants.
+    //they are also called as named constants .
+    //the names of variable are representing integers . default it starts from 0.
+    enum Month
+    {
+        january = 1,
+        february,
+        march,
+        april,
+        may,
+        june,
+        july,
+        august,
+        september,
+        october,
+        november,
+        december
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POConEnum
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the Number of Month : ");
+
+            int monthNumber=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"The Month is : {(Month)monthNumber}");
+        }
+    }
+}
